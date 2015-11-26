139e488ff4accd5271bfe8fb542341eedd3075f0
Johnm95 <johnmcmillan@Johns-MacBook-Pro.local>
2015-11-26 14:26:10 +0000

    Base

---
+++ builds/GP2CourseWork.build/Debug/ALL_BUILD.build/Script-915CE4D9F6574467BFF62A5B.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/johnmcmillan/GP2CourseWork/builds -f /Users/johnmcmillan/GP2CourseWork/builds/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
---
+++ builds/GP2CourseWork.build/Debug/ZERO_CHECK.build/Script-ABDBAF73F60F477180689095.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/johnmcmillan/GP2CourseWork/builds -f /Users/johnmcmillan/GP2CourseWork/builds/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
